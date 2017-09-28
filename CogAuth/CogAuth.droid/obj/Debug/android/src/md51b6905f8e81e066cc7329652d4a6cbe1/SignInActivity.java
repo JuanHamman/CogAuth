ff6c93fc36951b35783e14b80b72a9823108fbcb
Juan Hamman <juanhamman0@gmail.com>
2017-09-28 15:02:17 +0200

    Added the project

---
+++ CogAuth/CogAuth.droid/obj/Debug/android/src/md51b6905f8e81e066cc7329652d4a6cbe1/SignInActivity.java
@@ -0,0 +1,47 @@
+package md51b6905f8e81e066cc7329652d4a6cbe1;
+
+
+public class SignInActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("CogAuth.droid.Activities.SignInActivity, CogAuth.droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", SignInActivity.class, __md_methods);
+	}
+
+
+	public SignInActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == SignInActivity.class)
+			mono.android.TypeManager.Activate ("CogAuth.droid.Activities.SignInActivity, CogAuth.droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
