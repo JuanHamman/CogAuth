429c89c2642a34b5799ad1c9bfa7355d84f77bfb
Frederick Kobo <frederick@tangentsolutions.co.za>
2017-09-29 14:57:35 +0200

    2nd iteration

--- CogAuth/CogAuth.droid/Resources/Resource.Designer.cs
+++ CogAuth/CogAuth.droid/Resources/Resource.Designer.cs
@@ -49,6 +49,9 @@ namespace CogAuth.droid
 			// aapt resource value: 0x7f020000
 			public const int button_transparent_white_border = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int logo = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
--- CogAuth/CogAuth.droid/obj/Debug/android/src/cogauth/droid/cogauth/droid/R.java
+++ CogAuth/CogAuth.droid/obj/Debug/android/src/cogauth/droid/cogauth/droid/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int button_transparent_white_border=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
         public static final int btnCapture=0x7f050001;
