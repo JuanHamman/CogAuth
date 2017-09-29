1353f4ffa40eb34c7b0c9ab32e1ef07287aa3db0
Frederick Kobo <frederick@tangentsolutions.co.za>
2017-09-29 19:48:22 +0200

    the success screen styling

--- CogAuth/CogAuth.droid/Resources/Resource.Designer.cs
+++ CogAuth/CogAuth.droid/Resources/Resource.Designer.cs
@@ -90,16 +90,19 @@ namespace CogAuth.droid
 			public const int button_transparent_white_border = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int logo = 2130837509;
+			public const int happy = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int unicorn = 2130837510;
+			public const int logo = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int unicorn_pic = 2130837511;
+			public const int unicorn = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int wave = 2130837512;
+			public const int unicorn_pic = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int wave = 2130837513;
 			
 			static Drawable()
 			{
