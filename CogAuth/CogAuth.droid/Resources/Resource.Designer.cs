5f61a8f4ebe9bebf446d61ff64f2cf97413187b6
Frederick Kobo <frederick@tangentsolutions.co.za>
2017-09-29 17:10:38 +0200

    fix merge conflicts

--- CogAuth/CogAuth.droid/Resources/Resource.Designer.cs
+++ CogAuth/CogAuth.droid/Resources/Resource.Designer.cs
@@ -43,56 +43,112 @@ namespace CogAuth.droid
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050003
+			public const int accent = 2131034115;
+			
+			// aapt resource value: 0x7f050000
+			public const int orange = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int primary = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int primaryDark = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int window_background = 2131034116;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int button_filled_blue = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int button_filled_green = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int button_filled_orange = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int button_transparent_white_border = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int logo = 2130837508;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int btnCapture = 2130968577;
+			// aapt resource value: 0x7f070001
+			public const int btnCapture = 2131165185;
 			
-			// aapt resource value: 0x7f040002
-			public const int btnNext = 2130968578;
+			// aapt resource value: 0x7f070002
+			public const int btnNext = 2131165186;
 			
-			// aapt resource value: 0x7f040009
-			public const int btn_RegGuidlines_Guidelines = 2130968585;
+			// aapt resource value: 0x7f070009
+			public const int btn_RegGuidlines_Guidelines = 2131165193;
 			
-			// aapt resource value: 0x7f040003
-			public const int btn_RegSuccess_Next = 2130968579;
+			// aapt resource value: 0x7f070003
+			public const int btn_RegSuccess_Next = 2131165187;
 			
-			// aapt resource value: 0x7f040005
-			public const int btn_RegVoice_Next = 2130968581;
+			// aapt resource value: 0x7f070005
+			public const int btn_RegVoice_Next = 2131165189;
 			
-			// aapt resource value: 0x7f040004
-			public const int btn_RegVoice_Start = 2130968580;
+			// aapt resource value: 0x7f070004
+			public const int btn_RegVoice_Start = 2131165188;
 			
-			// aapt resource value: 0x7f040006
-			public const int btn_RegVoice_play = 2130968582;
+			// aapt resource value: 0x7f070006
+			public const int btn_RegVoice_play = 2131165190;
 			
-			// aapt resource value: 0x7f04000f
-			public const int btn_SignInVoice_Next = 2130968591;
+			// aapt resource value: 0x7f07000f
+			public const int btn_SignInVoice_Next = 2131165199;
 			
-			// aapt resource value: 0x7f04000e
-			public const int btn_SignInVoice_Play = 2130968590;
+			// aapt resource value: 0x7f07000e
+			public const int btn_SignInVoice_Play = 2131165198;
 			
-			// aapt resource value: 0x7f04000d
-			public const int btn_SignInVoice_Record = 2130968589;
+			// aapt resource value: 0x7f07000d
+			public const int btn_SignInVoice_Record = 2131165197;
 			
-			// aapt resource value: 0x7f04000b
-			public const int btn_SignIn_Register = 2130968587;
+			// aapt resource value: 0x7f07000b
+			public const int btn_SignIn_Register = 2131165195;
 			
-			// aapt resource value: 0x7f04000a
-			public const int btn_SignIn_SignIn = 2130968586;
+			// aapt resource value: 0x7f07000a
+			public const int btn_SignIn_SignIn = 2131165194;
 			
-			// aapt resource value: 0x7f04000c
-			public const int btn_signInSuccess_Next = 2130968588;
+			// aapt resource value: 0x7f07000c
+			public const int btn_signInSuccess_Next = 2131165196;
 			
-			// aapt resource value: 0x7f040008
-			public const int edt_RegGuidelines_Username = 2130968584;
+			// aapt resource value: 0x7f070008
+			public const int edt_RegGuidelines_Username = 2131165192;
 			
-			// aapt resource value: 0x7f040000
-			public const int imgPhoto = 2130968576;
+			// aapt resource value: 0x7f070000
+			public const int imgPhoto = 2131165184;
 			
-			// aapt resource value: 0x7f040007
-			public const int txt_RegGuidelines_GuideLines = 2130968583;
+			// aapt resource value: 0x7f070007
+			public const int txt_RegGuidelines_GuideLines = 2131165191;
 			
 			static Id()
 			{
@@ -107,29 +163,29 @@ namespace CogAuth.droid
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Registration_Face = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Registration_Face = 2130903040;
 			
-			// aapt resource value: 0x7f020001
-			public const int Registration_Success = 2130837505;
+			// aapt resource value: 0x7f030001
+			public const int Registration_Success = 2130903041;
 			
-			// aapt resource value: 0x7f020002
-			public const int Registration_Voice = 2130837506;
+			// aapt resource value: 0x7f030002
+			public const int Registration_Voice = 2130903042;
 			
-			// aapt resource value: 0x7f020003
-			public const int RegistrationGuidelines = 2130837507;
+			// aapt resource value: 0x7f030003
+			public const int RegistrationGuidelines = 2130903043;
 			
-			// aapt resource value: 0x7f020004
-			public const int SignIn = 2130837508;
+			// aapt resource value: 0x7f030004
+			public const int SignIn = 2130903044;
 			
-			// aapt resource value: 0x7f020005
-			public const int SignIn_Face = 2130837509;
+			// aapt resource value: 0x7f030005
+			public const int SignIn_Face = 2130903045;
 			
-			// aapt resource value: 0x7f020006
-			public const int SignIn_Success = 2130837510;
+			// aapt resource value: 0x7f030006
+			public const int SignIn_Success = 2130903046;
 			
-			// aapt resource value: 0x7f020007
-			public const int SignIn_Voice = 2130837511;
+			// aapt resource value: 0x7f030007
+			public const int SignIn_Voice = 2130903047;
 			
 			static Layout()
 			{
@@ -144,14 +200,14 @@ namespace CogAuth.droid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030001
-			public const int ApplicationName = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int ApplicationName = 2130968577;
 			
-			// aapt resource value: 0x7f030000
-			public const int Hello = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Hello = 2130968576;
 			
-			// aapt resource value: 0x7f030002
-			public const int app_name = 2130903042;
+			// aapt resource value: 0x7f040002
+			public const int app_name = 2130968578;
 			
 			static String()
 			{
@@ -159,6 +215,31 @@ namespace CogAuth.droid
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060003
+			public const int BlueButtonStyle = 2131099651;
+			
+			// aapt resource value: 0x7f060002
+			public const int GreenButtonStyle = 2131099650;
+			
+			// aapt resource value: 0x7f060001
+			public const int OrangeButtonStyle = 2131099649;
+			
+			// aapt resource value: 0x7f060000
+			public const int TransparentButtonStyle = 2131099648;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
--- CogAuth/CogAuth.droid/obj/Debug/android/cogauth/droid/cogauth/droid/R.java
+++ CogAuth/CogAuth.droid/obj/Debug/android/cogauth/droid/cogauth/droid/R.java
@@ -10,37 +10,57 @@ package cogauth.droid.cogauth.droid;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int accent=0x7f050003;
+        public static final int orange=0x7f050000;
+        public static final int primary=0x7f050001;
+        public static final int primaryDark=0x7f050002;
+        public static final int window_background=0x7f050004;
+    }
+    public static final class drawable {
+        public static final int button_filled_blue=0x7f020000;
+        public static final int button_filled_green=0x7f020001;
+        public static final int button_filled_orange=0x7f020002;
+        public static final int button_transparent_white_border=0x7f020003;
+        public static final int logo=0x7f020004;
+    }
     public static final class id {
-        public static final int btnCapture=0x7f040001;
-        public static final int btnNext=0x7f040002;
-        public static final int btn_RegGuidlines_Guidelines=0x7f040009;
-        public static final int btn_RegSuccess_Next=0x7f040003;
-        public static final int btn_RegVoice_Next=0x7f040005;
-        public static final int btn_RegVoice_Start=0x7f040004;
-        public static final int btn_RegVoice_play=0x7f040006;
-        public static final int btn_SignInVoice_Next=0x7f04000f;
-        public static final int btn_SignInVoice_Play=0x7f04000e;
-        public static final int btn_SignInVoice_Record=0x7f04000d;
-        public static final int btn_SignIn_Register=0x7f04000b;
-        public static final int btn_SignIn_SignIn=0x7f04000a;
-        public static final int btn_signInSuccess_Next=0x7f04000c;
-        public static final int edt_RegGuidelines_Username=0x7f040008;
-        public static final int imgPhoto=0x7f040000;
-        public static final int txt_RegGuidelines_GuideLines=0x7f040007;
+        public static final int btnCapture=0x7f070001;
+        public static final int btnNext=0x7f070002;
+        public static final int btn_RegGuidlines_Guidelines=0x7f070009;
+        public static final int btn_RegSuccess_Next=0x7f070003;
+        public static final int btn_RegVoice_Next=0x7f070005;
+        public static final int btn_RegVoice_Start=0x7f070004;
+        public static final int btn_RegVoice_play=0x7f070006;
+        public static final int btn_SignInVoice_Next=0x7f07000f;
+        public static final int btn_SignInVoice_Play=0x7f07000e;
+        public static final int btn_SignInVoice_Record=0x7f07000d;
+        public static final int btn_SignIn_Register=0x7f07000b;
+        public static final int btn_SignIn_SignIn=0x7f07000a;
+        public static final int btn_signInSuccess_Next=0x7f07000c;
+        public static final int edt_RegGuidelines_Username=0x7f070008;
+        public static final int imgPhoto=0x7f070000;
+        public static final int txt_RegGuidelines_GuideLines=0x7f070007;
     }
     public static final class layout {
-        public static final int registration_face=0x7f020000;
-        public static final int registration_success=0x7f020001;
-        public static final int registration_voice=0x7f020002;
-        public static final int registrationguidelines=0x7f020003;
-        public static final int signin=0x7f020004;
-        public static final int signin_face=0x7f020005;
-        public static final int signin_success=0x7f020006;
-        public static final int signin_voice=0x7f020007;
+        public static final int registration_face=0x7f030000;
+        public static final int registration_success=0x7f030001;
+        public static final int registration_voice=0x7f030002;
+        public static final int registrationguidelines=0x7f030003;
+        public static final int signin=0x7f030004;
+        public static final int signin_face=0x7f030005;
+        public static final int signin_success=0x7f030006;
+        public static final int signin_voice=0x7f030007;
     }
     public static final class string {
-        public static final int ApplicationName=0x7f030001;
-        public static final int Hello=0x7f030000;
-        public static final int app_name=0x7f030002;
+        public static final int ApplicationName=0x7f040001;
+        public static final int Hello=0x7f040000;
+        public static final int app_name=0x7f040002;
+    }
+    public static final class style {
+        public static final int BlueButtonStyle=0x7f060003;
+        public static final int GreenButtonStyle=0x7f060002;
+        public static final int OrangeButtonStyle=0x7f060001;
+        public static final int TransparentButtonStyle=0x7f060000;
     }
 }
